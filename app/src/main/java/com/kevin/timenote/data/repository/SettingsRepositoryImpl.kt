9b6c28c6676824c6c9dd92c5730e22056c304ad9
student9128 <jingpengchn@163.com>
2025-12-19 16:13:03 +0800

    optimize:optimize code

--- app/src/main/java/com/kevin/timenote/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/kevin/timenote/data/repository/SettingsRepositoryImpl.kt
@@ -6,20 +6,20 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.kevin.timenote.domain.model.ThemeMode
 import com.kevin.timenote.domain.repository.SettingsRepository
+import com.kevin.timenote.extension.dataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-val Context.dataStore by preferencesDataStore(name = "settings")
 class SettingsRepositoryImpl@Inject constructor(
     @ApplicationContext private val context: Context,
 )  : SettingsRepository {
-    private val ThemeModeKey = stringPreferencesKey("ThemeMode")
+    private val themeModeKey = stringPreferencesKey("ThemeMode")
 
     // 1. 读取流：监听设置的变化
     override val themeMode: Flow<ThemeMode> = context.dataStore.data.map { preferences ->
-        val modeName = preferences[ThemeModeKey] ?: ThemeMode.Light.name
+        val modeName = preferences[themeModeKey] ?: ThemeMode.Light.name
         try {
             ThemeMode.valueOf(modeName)
         } catch (e: Exception) {
@@ -29,7 +29,7 @@ class SettingsRepositoryImpl@Inject constructor(
 
     override suspend fun changeThemeMode(mode: ThemeMode) {
         context.dataStore.edit { preferences ->
-            preferences[ThemeModeKey]=mode.name
+            preferences[themeModeKey]=mode.name
         }
     }
 }
--- app/src/main/java/com/kevin/timenote/extension/ContextExt.kt
+++ app/src/main/java/com/kevin/timenote/extension/ContextExt.kt
@@ -1,2 +1,6 @@
 package com.kevin.timenote.extension
 
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore by preferencesDataStore(name = "settings")
