af3ee650a60f7df7d9ba60720066c8e3ee570d77
student9128 <jingpengchn@163.com>
2025-12-24 10:59:13 +0800

    add:add Switch

---
+++ app/src/main/java/com/kevin/timenote/ui/widget/TimeSwitch.kt
@@ -0,0 +1,29 @@
+package com.kevin.timenote.ui.widget
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun TimeSwitch(onCheckedChange: (v: Boolean) -> Unit) {
+    var checked by remember { mutableStateOf(false) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            onCheckedChange(it)
+            checked = it
+        },
+        colors = SwitchDefaults.colors(
+            checkedThumbColor = MaterialTheme.colorScheme.primary,
+            checkedTrackColor = MaterialTheme.colorScheme.primaryContainer,
+            uncheckedThumbColor = MaterialTheme.colorScheme.secondary,
+            uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+        )
+    )
+}
